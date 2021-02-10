d9cf09c7833d66f5abf1108c239d0f532d6f763a
Anastasia <n41111215@mail.ru>
2021-02-10 15:48:15 +0300

    add shouldSendFormWithValidData

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    LocalDate today = LocalDate.now();
+    LocalDate newDate = today.plusDays(3);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    @BeforeEach
+    void Setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSendFormWithValidData() {
+        SelenideElement form = $(".form");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79258135366");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
+    }
+
+}
